bfd2ebf2f87f8e2b51bccf90ab5941a60f42e360
esterr <esterr@3basegroup.com>
2013-07-24 09:08:27 +0300

    filter by template type

--- tmpls/admin.py
+++ tmpls/admin.py
@@ -4,7 +4,8 @@ from tmpls_types.models import Type
 
 class TemplateAdmin(admin.ModelAdmin):
 	list_display = ('name', 'templateType', 'isPublish', 'isPublic')
-	list_filter = ['templateType', 'isPublish', 'isPublic']
+	list_filter = ('templateType__name', 'isPublish', 'isPublic')
+	search_fields = ['name']
 
 admin.site.register(Template, TemplateAdmin)
 
