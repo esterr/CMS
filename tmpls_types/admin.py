f5930d7a08240760c7919a4959ff57120fbbdc69
esterr <esterr@3basegroup.com>
2013-07-24 14:08:50 +0300

    template type layout

--- tmpls_types/admin.py
+++ tmpls_types/admin.py
@@ -4,14 +4,31 @@ from django.db import models
 
 from tmpls_types.models import Type
 
+from django import forms
+class TemplateTypeModelForm( forms.ModelForm ):
+	layout = forms.CharField( widget=forms.Textarea(attrs={'size': '200', 'cols':80}) )
+	class Meta:
+		model = Type
+
+
 class TemplateTypeAdmin(admin.ModelAdmin):
+	form = TemplateTypeModelForm
 	fieldsets = [
-        (None, {'fields': ['name', 'description']}),
+        (None, {'fields': ['name', 'description', 'layout']}),
     ]
 	list_display = ('id', 'name', 'description')
 	# list_filter = ['name']
 	search_fields = ['name']
 
+	def save_model(self, request, form, formset, change):
+		super(TemplateTypeAdmin, self).save_model(request, form, formset, change)
+
+		fileName = "_".join(form.name.lower().split(" "))
+		f = open("tmpls_types\\templates\\" + fileName + ".html", "w+")
+		try:
+			f.write(form.layout) #Create the layout file
+		finally:
+			f.close()
 
 admin.site.register(Type, TemplateTypeAdmin)
 		
--- tmpls_types/models.py
+++ tmpls_types/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Type(models.Model):
 	name = models.CharField(max_length=100)
-	description = models.CharField(max_length=300)
+	description = models.CharField(max_length=300, null=True)
+	layout = models.CharField(max_length=1000)
 
 	def __str__(self):
 		return self.name
