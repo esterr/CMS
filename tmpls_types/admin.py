ebb757166ae4f78bc10cc5e3b2e308ace8cf60e8
esterr <esterr@3basegroup.com>
2013-07-24 15:09:06 +0300

    layout

--- tmpls_types/admin.py
+++ tmpls_types/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.contrib import messages
 from django.db import models
+from django import forms
 
 from tmpls_types.models import Type
 
-from django import forms
 class TemplateTypeModelForm( forms.ModelForm ):
-	layout = forms.CharField( widget=forms.Textarea(attrs={'size': '200', 'cols':80}) )
+
+	# get layout file contet into "layout" field
+	def __init__(self, *args, **kwargs):
+		super(TemplateTypeModelForm, self).__init__(*args, **kwargs)
+
+		fileName = self.instance.name.replace(" ", "_").lower()
+		try:
+			f = open("tmpls_types\\templates\\" + self.instance.name + ".html", "r")
+			content = f.read() #Get layout file content
+			f.close()
+		except IOError:
+			content = ""
+			
+		self.initial['layout'] = content
+
+	# layout field as textarea
+	layout = forms.CharField( widget = forms.Textarea(attrs = {'size': '200', 'cols':80}) )
 	class Meta:
 		model = Type
 
@@ -17,14 +33,14 @@ class TemplateTypeAdmin(admin.ModelAdmin):
         (None, {'fields': ['name', 'description', 'layout']}),
     ]
 	list_display = ('id', 'name', 'description')
-	# list_filter = ['name']
 	search_fields = ['name']
 
+	# save the layout field into file
 	def save_model(self, request, form, formset, change):
 		super(TemplateTypeAdmin, self).save_model(request, form, formset, change)
 
-		fileName = "_".join(form.name.lower().split(" "))
-		f = open("tmpls_types\\templates\\" + fileName + ".html", "w+")
+		fileName = form.name.replace(" ", "_").lower()
+		f = open("tmpls_types\\templates\\" + fileName + ".html", "w")
 		try:
 			f.write(form.layout) #Create the layout file
 		finally:
